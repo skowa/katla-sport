8404a3c0a93f2bed46ca10d7ca0d87a28812f065
Alexandra Gushchenskova <skowa98@gmail.com>
2019-01-23 17:06:07 +0300

    Delete and Restore buttons are added to Hive List and Hive Section List pages

--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -16,7 +17,8 @@ export class HiveSectionListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private hiveService: HiveService
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
   ) { }
 
   ngOnInit() {
@@ -24,5 +26,18 @@ export class HiveSectionListComponent implements OnInit {
       this.hiveId = p['id'];
       this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
     })
+  }
+
+  onDelete(hiveSectionId: number) {
+    this.setDeletedStatus(hiveSectionId, true);
+  }
+
+  onRestore(hiveSectionId: number) {
+    this.setDeletedStatus(hiveSectionId, false);
+  }
+
+  private setDeletedStatus(hiveSectionId: number, newDeletedStatus: boolean) {
+    var hiveSection = this.hiveSections.find(h => h.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, newDeletedStatus).subscribe(c => hiveSection.isDeleted = newDeletedStatus);
   }
 }
