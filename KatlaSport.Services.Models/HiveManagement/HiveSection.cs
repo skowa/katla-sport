a4e0ff3de8cb7ca88f0831ca5b749ead72029dc2
Alexandra Gushchenskova <skowa98@gmail.com>
2019-01-27 14:41:05 +0300

    Add StoreHiveId to HiveSection, StoreHiveId check to HiveSectionService.

--- KatlaSport.Services.Models/HiveManagement/HiveSection.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSection.cs
@@ -8,6 +8,11 @@ namespace KatlaSport.Services.HiveManagement
     public class HiveSection : HiveSectionListItem
     {
         /// <summary>
+        /// Gets or sets the hive id of hive section.
+        /// </summary>
+        public int StoreHiveId { get; set; }
+
+        /// <summary>
         /// Gets or sets a timestamp when the hive section was updated last time.
         /// </summary>
         public DateTime LastUpdated { get; set; }
